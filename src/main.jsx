60046f9e45fda0923bdb42633a21f5cf93290e1b
luigi138 <nimapi20041012@gmail.com>
2023-09-05 19:41:59 -0300

    segundo main

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,29 @@ import Home from './components/Home.jsx';
 import Produtos from './components/Produtos.jsx';
 import EditarProduto from './components/EditarProduto.jsx';
 import Error from './components/Error.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProduto/>
+      },
+      {
+        path: "/antiga",
+        element: <Navigate to="/"/>
+      }
+    ]
+  }
+])
