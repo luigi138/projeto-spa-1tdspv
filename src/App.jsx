cc6a5b5cee8d10e0eac65a0b4ef7b1eed3b2ed4d
luigi138 <nimapi20041012@gmail.com>
2023-09-05 19:44:35 -0300

    comeco do app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-
-export default function App() {
- 
- //Area decrarativa
- 
-  return (
-    <>
-    {/* Area imperativa! */}
-    <div>
-     <h1>Vite + React</h1>
-     <h2>Conseguimos!</h2>
-     <p>Ola Mundo!</p>
-    </div>
-
-    </>
-  )
-}
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import Menu from './components/Menu'
-import Rodape from './components/Rodape'
 
 function App() {
 
   return (
     <>
-    <Menu/>
+    
     <Outlet/>
-    <Rodape/>
+    
     </>
   )
 }
