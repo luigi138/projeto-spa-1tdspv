97f6df6bcb291b61538765a64a82645e5ef452b1
luigi138 <nimapi20041012@gmail.com>
2023-09-05 19:41:04 -0300

    comeco do main

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
 import { createBrowserRouter, RouberProvider, Navigate } from 'react-router-dom'
 import Home from './components/Home.jsx';
 import Produtos from './components/Produtos.jsx';
 import EditarProduto from './components/EditarProduto.jsx';
 import Error from './components/Error.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    errorElement: <Error/>,
-    children:[
-      {
-        path: "/",
-        element: <Home/>
-      },
-      {
-        path: "/produtos",
-        element: <Produtos/>
-      },
-      {
-        path: "/produtos/editar/:id",
-        element: <EditarProduto/>
-      },
-      {
-        path: "/antiga",
-        element: <Navigate to="/"/>
-      }
-    ]
-  }
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouberProvider rouder={router} />
-  </React.StrictMode>,
-)
-
